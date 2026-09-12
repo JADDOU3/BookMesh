60fb2279a486db9221f4d67fb156b166f53a34c1
JADDOU3 <135611544+JADDOU3@users.noreply.github.com>
2026-09-12 23:37:36 +0300

    init commit

---
+++ src/main/java/org/example/bookmesh/BookMeshApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookmesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMeshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMeshApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bookmesh/BookMeshApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookmesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMeshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
