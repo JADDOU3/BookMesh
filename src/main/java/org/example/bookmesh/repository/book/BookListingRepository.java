328863555919525e66e93e1567a4240094165f85
JADDOU3 <135611544+JADDOU3@users.noreply.github.com>
2026-09-16 22:32:08 +0300

    Replace JPQL -> Entity Graph

--- src/main/java/org/example/bookmesh/repository/book/BookListingRepository.java
+++ src/main/java/org/example/bookmesh/repository/book/BookListingRepository.java
@@ -1,20 +1,18 @@
 package org.example.bookmesh.repository.book;
 
 import org.example.bookmesh.model.book.BookListing;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface BookListingRepository extends JpaRepository<BookListing, Long> {
 
-    @Query("select l from BookListing l join fetch l.book join fetch l.supplier where l.book.id = :bookId")
-    List<BookListing> findByBookId(@Param("bookId") Long bookId);
+    @EntityGraph(attributePaths = {"book", "supplier"})
+    List<BookListing> findByBookId(Long bookId);
 
-    @Query("select l from BookListing l join fetch l.book join fetch l.supplier where l.supplier.id = :supplierId")
-    List<BookListing> findBySupplierId(@Param("supplierId") Long supplierId);
+    @EntityGraph(attributePaths = {"book", "supplier"})
+    List<BookListing> findBySupplierId(Long supplierId);
 
     boolean existsByBookIdAndSupplierId(Long bookId, Long supplierId);
 }
--- src/main/java/org/example/bookmesh/repository/book/BookRepository.java
+++ src/main/java/org/example/bookmesh/repository/book/BookRepository.java
@@ -1,18 +1,17 @@
 package org.example.bookmesh.repository.book;
 
 import org.example.bookmesh.model.book.Book;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.graphql.data.federation.EntityMapping;
 
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    @Query("select b from Book b join fetch b.author where b.author.id = :authorId")
+    @EntityGraph(attributePaths = {"author"})
     List<Book> findByAuthorId(@Param("authorId") Long authorId);
 
-    @Override
-    @Query("select b from Book b join fetch b.author")
-    List<Book> findAll();
 }
